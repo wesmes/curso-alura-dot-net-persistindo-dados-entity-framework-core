b3bd8a9328c156a2d7c88102d2c6021565326c8f
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2024-09-22 06:46:42 -0300

    Módulo 2 - Aula 12: Persistindo músicas

---
+++ 3506-csharpWeb-screensound-curso1-main/ScreenSound/Banco/MusicaDAL.cs
@@ -0,0 +1,52 @@
+﻿using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Banco
+{
+    internal class MusicaDAL
+    {
+
+        private readonly ScreenSoundContext context;
+
+        public MusicaDAL(ScreenSoundContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Musica> Listar()
+        {
+            return context.Musicas.ToList();
+        }
+
+        public void Adicionar(Musica musica)
+        {
+            // Adição do "using" para determinar que a implementação da conexão
+            // será encerrada após execução do escopo de onde ela foi implementada
+            context.Musicas.Add(musica);
+            context.SaveChanges();
+        }
+
+        public void Atualizar(Musica musica)
+        {
+            context.Musicas.Update(musica);
+            context.SaveChanges();
+        }
+
+        public void Deletar(Musica musica)
+        {
+            context.Musicas.Remove(musica);
+            context.SaveChanges();
+        }
+
+        public Musica? RecuperarPeloNome(string nome)
+        {
+            return context.Musicas.FirstOrDefault(a => a.Nome.Equals(nome));
+        }
+
+    }
+}
--- 3506-csharpWeb-screensound-curso1-main/ScreenSound/Banco/ScreenSoundContext.cs
+++ 3506-csharpWeb-screensound-curso1-main/ScreenSound/Banco/ScreenSoundContext.cs
@@ -13,7 +13,9 @@ namespace ScreenSound.Banco
     {
 
         public DbSet<Artista> Artistas { get; set; }
-        
+        public DbSet<Musica> Musicas { get; set; }
+
+
         private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
