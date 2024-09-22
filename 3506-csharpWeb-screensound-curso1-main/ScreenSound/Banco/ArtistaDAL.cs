a946c4bf9422034d6bc007568a8d042add5dc9b6
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2024-09-22 07:09:51 -0300

    Módulo 3 - Aula 4: Implementado métodos genéricos

--- 3506-csharpWeb-screensound-curso1-main/ScreenSound/Banco/ArtistaDAL.cs
+++ 3506-csharpWeb-screensound-curso1-main/ScreenSound/Banco/ArtistaDAL.cs
@@ -11,37 +11,7 @@ namespace ScreenSound.Banco
     internal class ArtistaDAL: DAL<Artista>
     {
 
-        private readonly ScreenSoundContext context;
-
-        public ArtistaDAL(ScreenSoundContext context)
-        {
-            this.context = context;
-        }
-
-        public override IEnumerable<Artista> Listar()
-        {
-            return context.Artistas.ToList();
-        }
-
-        public override void Adicionar(Artista artista)
-        {
-            // Adição do "using" para determinar que a implementação da conexão
-            // será encerrada após execução do escopo de onde ela foi implementada
-            context.Artistas.Add(artista);
-            context.SaveChanges();
-        }
-
-        public override void Atualizar(Artista artista)
-        {
-            context.Artistas.Update(artista);
-            context.SaveChanges();
-        }
-
-        public override void Deletar(Artista artista)
-        {
-            context.Artistas.Remove(artista);
-            context.SaveChanges();
-        }
+        public ArtistaDAL(ScreenSoundContext context) : base(context) { }
 
         public Artista? RecuperarPeloNome(string nome)
         {
